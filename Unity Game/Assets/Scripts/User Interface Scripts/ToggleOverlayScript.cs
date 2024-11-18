8b47b19ed7ac0bf8adf2e06f657b5386edd373c7
Chime-txt <122952454+Chime-txt@users.noreply.github.com>
2024-11-17 19:44:43 -0600

    Credits + Toggle Overlay Script
    - Fixed the issue where the death planes are not connected to the players in level 1, which would cause the players to fall indefinitely
    - Added the credits with all of the assets we used
    - Added certain functions to the Toggle Overlay Script, although in the future, this can be used as a general function

--- Unity Game/Assets/Scripts/User Interface Scripts/ToggleOverlayScript.cs
+++ Unity Game/Assets/Scripts/User Interface Scripts/ToggleOverlayScript.cs
@@ -5,24 +5,64 @@ using UnityEngine;
 public class ToggleOverlayScript : MonoBehaviour
 {
 	[Header("UI Management")]
-	[SerializeField] GameObject OverlayToToggle;
+	[SerializeField] GameObject Overlay1;
+	[SerializeField] GameObject OptionalOverlay2;
 
 	public void ToggleOverlay()
 	{
 		// Check if overlay does not exist
-		if (OverlayToToggle == null)
+		if (Overlay1 == null)
 		{
 			return;
 		}
 
+		SwitchActiveState(Overlay1);
+
+		if (OptionalOverlay2 == null)
+		{
+			return;
+		}
+
+		SwitchActiveState(OptionalOverlay2);
+
+		return;
+	}
+
+	// Switch between states without knowing which state the overlay is in
+	private static void SwitchActiveState(GameObject Overlay)
+	{
 		// Switch overlay state
-		if (OverlayToToggle.activeInHierarchy == true)
+		if (Overlay.activeInHierarchy == true)
 		{
-			OverlayToToggle.SetActive(false);
+			Overlay.SetActive(false);
 		}
-		else if (OverlayToToggle.activeInHierarchy == false)
+		else if (Overlay.activeInHierarchy == false)
 		{
-			OverlayToToggle.SetActive(true);
+			Overlay.SetActive(true);
 		}
+
+		return;
+	}
+
+	// Disable any overlay
+	public void DisableOverlay(GameObject Overlay)
+	{
+		if (Overlay == null)
+		{
+			return;
+		}
+		Overlay.SetActive(false);
+		return;
+	}
+
+	// Enable any overlay
+	public void EnableOverlay(GameObject Overlay)
+	{
+		if (Overlay == null)
+		{
+			return;
+		}
+		Overlay.SetActive(true);
+		return;
 	}
 }
